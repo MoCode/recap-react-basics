96d4754974ca6e22d6483efe58004fb6a354d48f
Moritz Wright <me@moritzwright.com>
2019-11-15 12:47:59 +0100

    Updates

---
+++ precache-manifest.115f781fe79e53bb4fa6f8c57fe48b98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f922f99c1f1359d6611826b6496345",
+    "url": "/recap-react-basics/index.html"
+  },
+  {
+    "revision": "e1bffe586add44f444a6",
+    "url": "/recap-react-basics/static/css/main.9f4df4e3.chunk.css"
+  },
+  {
+    "revision": "74e9af04ce70d5b3addb",
+    "url": "/recap-react-basics/static/js/2.0c3a30c3.chunk.js"
+  },
+  {
+    "revision": "e1bffe586add44f444a6",
+    "url": "/recap-react-basics/static/js/main.a32ec02e.chunk.js"
+  },
+  {
+    "revision": "6bcd25fb41b079c58323",
+    "url": "/recap-react-basics/static/js/runtime-main.ab9e89cb.js"
+  }
+]);
